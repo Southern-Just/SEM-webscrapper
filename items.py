bdae4e49d1a52ab6647875bccb401aba0029dd4e
Southern-Just <karuguezekiel@gmail.com>
2025-01-30 22:33:13 +0300

    002 api config

--- items.py
+++ items.py
@@ -10,3 +10,12 @@ class NseScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+from scrapy.item import Item, Field
+
+
+class NseScraperItem(Item):
+   # define the fields for your item here like:
+   ticker_symbol = Field()
+   stock_name = Field()
+   stock_price = Field()
+   stock_change = Field()
